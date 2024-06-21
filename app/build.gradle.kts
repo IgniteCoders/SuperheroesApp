3d4516e8fab719d28dea615f5d473591babba7f8
Mansour <theboom.kajas@gmail.com>
2024-06-21 14:07:36 +0200

    Primeros pasos con Retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,11 @@ android {
 }
 
 dependencies {
+    // Retrofit
+    val retrofitVersion = "2.11.0"
+    implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
+    implementation("com.squareup.retrofit2:converter-gson:$retrofitVersion")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
 
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
---
+++ app/src/main/java/com/example/superheroesapp/data/SuperheroResponse.kt
@@ -0,0 +1,8 @@
+package com.example.superheroesapp.data
+
+import com.google.gson.annotations.SerializedName
+
+data class SuperheroResponse (
+    @SerializedName("name") val name: String
+) {
+}
