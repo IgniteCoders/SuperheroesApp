91031dbff9b4c0ce2f82bd35c22069be3e5ac6bd
Mansour <theboom.kajas@gmail.com>
2024-06-24 13:34:04 +0200

    Refactor

--- app/src/main/java/com/example/superheroesapp/data/SuperheroResponse.kt
+++ app/src/main/java/com/example/superheroesapp/data/SuperheroResponse.kt
@@ -2,7 +2,19 @@ package com.example.superheroesapp.data
 
 import com.google.gson.annotations.SerializedName
 
-data class SuperheroResponse (
-    @SerializedName("name") val name: String
+data class SuperheroResponse(
+    @SerializedName("response") val response: String,
+    @SerializedName("results-for") val resultsFor: String,
+    @SerializedName("results") val results: List<Superhero>
 ) {
 }
+
+data class Superhero (
+    @SerializedName("name") val name: String,
+    @SerializedName("image") val image: Image
+) {
+}
+
+data class Image (
+    @SerializedName("url") val url: String
+)
