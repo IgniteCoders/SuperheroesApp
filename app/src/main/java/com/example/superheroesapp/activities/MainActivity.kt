ad4ff1c97a98f916e6a5ab1707edbb2d6fb222e4
Mansour <theboom.kajas@gmail.com>
2024-06-24 13:53:16 +0200

    Hemos añadido la libreria Picasso para cargar las imagenes y hemos mejorado el diseño de la celda

--- app/src/main/java/com/example/superheroesapp/activities/MainActivity.kt
+++ app/src/main/java/com/example/superheroesapp/activities/MainActivity.kt
@@ -34,7 +34,7 @@ class MainActivity : AppCompatActivity() {
         binding.recyclerView.adapter = adapter
         binding.recyclerView.layoutManager = GridLayoutManager(this, 2)
 
-        searchByName("super")
+        searchByName("a")
     }
 
     private fun searchByName(query: String){
